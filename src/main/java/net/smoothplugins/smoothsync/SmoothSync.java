e095681fbfa8866fc44063fb418d0eada4162fa2
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-19 19:10:28 +0200

    Add mongo & redis connection

--- src/main/java/net/smoothplugins/smoothsync/SmoothSync.java
+++ src/main/java/net/smoothplugins/smoothsync/SmoothSync.java
@@ -3,7 +3,10 @@ package net.smoothplugins.smoothsync;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import net.smoothplugins.smoothbase.configuration.Configuration;
+import net.smoothplugins.smoothbase.connection.MongoConnection;
+import net.smoothplugins.smoothbase.connection.RedisConnection;
 import net.smoothplugins.smoothsync.module.ConfigurationModule;
+import net.smoothplugins.smoothsync.module.ConnectionModule;
 import net.smoothplugins.smoothsync.module.SmoothSyncModule;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -17,9 +20,20 @@ public final class SmoothSync extends JavaPlugin {
         Configuration config = new Configuration(this, "config");
         Configuration messages = new Configuration(this, "messages");
 
+        String uri = config.getString("mongo.uri");
+        String databaseName = config.getString("mongo.database");
+        MongoConnection mongoConnection = new MongoConnection(uri, databaseName);
+
+        String redisHost = config.getString("redis.host");
+        int redisPort = config.getInt("redis.port");
+        String redisPassword = config.getString("redis.password");
+        String redisPrefix = config.getString("redis.cluster");
+        RedisConnection redisConnection = new RedisConnection(redisHost, redisPort, redisPassword, redisPrefix);
+
         injector = Guice.createInjector(
                 new SmoothSyncModule(this),
-                new ConfigurationModule(config, messages)
+                new ConfigurationModule(config, messages),
+                new ConnectionModule(mongoConnection, redisConnection)
         );
     }
 
