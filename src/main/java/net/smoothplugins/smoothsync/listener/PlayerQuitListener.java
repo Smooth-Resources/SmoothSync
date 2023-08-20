8e0a8fb0cea1f2d01ca8c16128dd87a940e005a8
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-20 22:29:10 +0200

    Update quit listener

--- src/main/java/net/smoothplugins/smoothsync/listener/PlayerQuitListener.java
+++ src/main/java/net/smoothplugins/smoothsync/listener/PlayerQuitListener.java
@@ -5,6 +5,7 @@ import net.smoothplugins.smoothsync.SmoothSync;
 import net.smoothplugins.smoothsyncapi.service.Destination;
 import net.smoothplugins.smoothsyncapi.user.User;
 import net.smoothplugins.smoothsyncapi.user.UserService;
+import net.smoothplugins.smoothsyncapi.user.UserTranslator;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,25 +13,24 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import java.time.LocalDateTime;
-
 public class PlayerQuitListener implements Listener {
 
     @Inject
     private UserService userService;
     @Inject
     private SmoothSync plugin;
+    @Inject
+    private UserTranslator userTranslator;
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onQuit(PlayerQuitEvent event) {
-        System.out.println("quit: " + LocalDateTime.now());
         Player player = event.getPlayer();
 
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
             User user = userService.getUserByUUID(player.getUniqueId()).orElse(null);
             if (user == null) return;
 
-            user.setItems(player.getInventory().getContents());
+            user = userTranslator.translateToUser(user, player);
             userService.update(user, Destination.CACHE, Destination.STORAGE);
             userService.setTTLOfCacheByUUID(user.getUuid(), 600); // 10 minutes
         });
