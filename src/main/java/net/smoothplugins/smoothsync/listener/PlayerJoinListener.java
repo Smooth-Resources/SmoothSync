96e8a681e2f9b92c6f2e158fefc6ba86728f25e0
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-20 22:41:01 +0200

    Update player join listener

--- src/main/java/net/smoothplugins/smoothsync/listener/PlayerJoinListener.java
+++ src/main/java/net/smoothplugins/smoothsync/listener/PlayerJoinListener.java
@@ -6,14 +6,13 @@ import net.smoothplugins.smoothbase.configuration.Configuration;
 import net.smoothplugins.smoothsync.SmoothSync;
 import net.smoothplugins.smoothsyncapi.user.User;
 import net.smoothplugins.smoothsyncapi.user.UserService;
+import net.smoothplugins.smoothsyncapi.user.UserTranslator;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-
-import java.time.LocalDateTime;
 
 public class PlayerJoinListener implements Listener {
 
@@ -23,19 +22,27 @@ public class PlayerJoinListener implements Listener {
     private SmoothSync plugin;
     @Inject @Named("config")
     private Configuration config;
+    @Inject
+    private UserTranslator userTranslator;
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onJoin(PlayerJoinEvent event) {
-        System.out.println("join: " + LocalDateTime.now());
         Player player = event.getPlayer();
+        if (!userService.containsByUUID(player.getUniqueId())) {
+            User user = new User(player.getUniqueId());
+            userTranslator.translateToUser(user, player);
+            userService.create(user);
+            userService.loadToCache(user);
+        }
 
-        // TODO: Hacer que al guardar cada X tiempo no se guarde justo ahora.
+        // TODO: Hacer que al guardar cada X tiempo no se guarde justo ahora. Limpiar más cosas del user.
         player.getInventory().clear();
 
         Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, () -> {
-            System.out.println("join-sync: " + LocalDateTime.now());
             User user = userService.getUserByUUID(player.getUniqueId()).orElseGet(() -> {
-                User newUser = new User(player.getUniqueId(), player.getInventory().getContents());
+                // This code never runs, but it's here just in case.
+                User newUser = new User(player.getUniqueId());
+                userTranslator.translateToUser(newUser, player);
                 userService.create(newUser);
                 return newUser;
             });
@@ -45,9 +52,9 @@ public class PlayerJoinListener implements Listener {
             }
 
             Bukkit.getScheduler().runTask(plugin, () -> {
-                player.getInventory().setContents(user.getItems());
-                player.sendMessage("Synced your inventory with the database.");
+                userTranslator.translateToPlayer(user, player);
             });
+
         },  (int) ((config.getInt("synchronization.join-delay") / 1000F) * 20));
     }
 }
