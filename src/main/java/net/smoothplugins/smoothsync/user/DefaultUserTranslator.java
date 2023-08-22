63ecc421b657d2cb8fcd566ae4a9d4e94d676073
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-23 00:25:21 +0200

    Update default user translator

--- src/main/java/net/smoothplugins/smoothsync/user/DefaultUserTranslator.java
+++ src/main/java/net/smoothplugins/smoothsync/user/DefaultUserTranslator.java
@@ -7,12 +7,15 @@ import net.smoothplugins.smoothsync.SmoothSync;
 import net.smoothplugins.smoothsyncapi.user.User;
 import net.smoothplugins.smoothsyncapi.user.UserTranslator;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.Statistic;
 import org.bukkit.advancement.Advancement;
 import org.bukkit.advancement.AdvancementProgress;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
+import java.util.Collection;
 import java.util.HashMap;
 
 public class DefaultUserTranslator implements UserTranslator {
@@ -38,17 +41,58 @@ public class DefaultUserTranslator implements UserTranslator {
         user.setRemainingAir(player.getRemainingAir());
         user.setLocation(player.getLocation());
 
-        HashMap<Advancement, AdvancementProgress> advancements = new HashMap<>();
+        HashMap<Advancement, Collection<String>> advancements = new HashMap<>();
         Bukkit.getServer().advancementIterator().forEachRemaining(advancement -> {
-            advancements.put(advancement, player.getAdvancementProgress(advancement));
+            AdvancementProgress progress = player.getAdvancementProgress(advancement);
+            if (progress.getRemainingCriteria().size() == advancement.getCriteria().size()) return;
+
+            advancements.put(advancement, progress.getAwardedCriteria());
         });
         user.setAdvancements(advancements);
 
-        HashMap<Statistic, Integer> statistics = new HashMap<>();
+        HashMap<Statistic, Integer> globalStatistics = new HashMap<>();
+        HashMap<Statistic, HashMap<Material, Integer>> blockStatistics = new HashMap<>();
+        HashMap<Statistic, HashMap<EntityType, Integer>> entityStatistics = new HashMap<>();
         for (Statistic statistic : Statistic.values()) {
-            statistics.put(statistic, player.getStatistic(statistic));
+            switch (statistic) {
+                case DROP, PICKUP, MINE_BLOCK, USE_ITEM, BREAK_ITEM, CRAFT_ITEM -> {
+                    HashMap<Material, Integer> materialIntegerHashMap = new HashMap<>();
+                    for (Material material : Material.values()) {
+                        int amount = player.getStatistic(statistic, material);
+                        if (amount > 0) {
+                            materialIntegerHashMap.put(material, amount);
+                        }
+                    }
+
+                    blockStatistics.put(statistic, materialIntegerHashMap);
+                }
+
+                case KILL_ENTITY, ENTITY_KILLED_BY -> {
+                    HashMap<EntityType, Integer> entityTypeIntegerHashMap = new HashMap<>();
+                    for (EntityType entityType : EntityType.values()) {
+                        if (entityType == EntityType.UNKNOWN) continue;
+
+                        int amount = player.getStatistic(statistic, entityType);
+                        if (amount > 0) {
+                            entityTypeIntegerHashMap.put(entityType, amount);
+                        }
+                    }
+
+                    entityStatistics.put(statistic, entityTypeIntegerHashMap);
+                }
+
+                default -> {
+                    int amount = player.getStatistic(statistic);
+                    if (amount > 0) {
+                        globalStatistics.put(statistic, amount);
+                    }
+                }
+            }
         }
-        user.setStatistics(statistics);
+
+        user.setGlobalStatistics(globalStatistics);
+        user.setBlockStatistics(blockStatistics);
+        user.setEntityStatistics(entityStatistics);
     }
 
     @Override
@@ -114,13 +158,26 @@ public class DefaultUserTranslator implements UserTranslator {
         if (section.getBoolean("advancements")) {
             user.getAdvancements().keySet().forEach(advancement -> {
                 AdvancementProgress progress = player.getAdvancementProgress(advancement);
-                progress = user.getAdvancements().get(advancement);
+                advancement.getCriteria().forEach(progress::revokeCriteria);
+                user.getAdvancements().get(advancement).forEach(progress::awardCriteria);
             });
         }
 
         if (section.getBoolean("statistics")) {
-            user.getStatistics().keySet().forEach(statistic -> {
-                player.setStatistic(statistic, user.getStatistics().get(statistic));
+            user.getGlobalStatistics().keySet().forEach(statistic -> {
+                player.setStatistic(statistic, user.getGlobalStatistics().get(statistic));
+            });
+
+            user.getBlockStatistics().keySet().forEach(statistic -> {
+                user.getBlockStatistics().get(statistic).keySet().forEach(material -> {
+                    player.setStatistic(statistic, material, user.getBlockStatistics().get(statistic).get(material));
+                });
+            });
+
+            user.getEntityStatistics().keySet().forEach(statistic -> {
+                user.getEntityStatistics().get(statistic).keySet().forEach(entityType -> {
+                    player.setStatistic(statistic, entityType, user.getEntityStatistics().get(statistic).get(entityType));
+                });
             });
         }
     }
