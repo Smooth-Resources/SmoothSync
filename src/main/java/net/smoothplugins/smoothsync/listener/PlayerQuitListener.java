1f4bec2fd4da49f4f31ecad195a2d3f3f4d56556
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-24 18:51:29 +0200

    Update quit listener

--- src/main/java/net/smoothplugins/smoothsync/listener/PlayerQuitListener.java
+++ src/main/java/net/smoothplugins/smoothsync/listener/PlayerQuitListener.java
@@ -2,6 +2,7 @@ package net.smoothplugins.smoothsync.listener;
 
 import com.google.inject.Inject;
 import net.smoothplugins.smoothsync.SmoothSync;
+import net.smoothplugins.smoothsyncapi.event.DataUpdateEvent;
 import net.smoothplugins.smoothsyncapi.service.Destination;
 import net.smoothplugins.smoothsyncapi.user.User;
 import net.smoothplugins.smoothsyncapi.user.UserService;
@@ -12,6 +13,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class PlayerQuitListener implements Listener {
 
@@ -30,8 +34,13 @@ public class PlayerQuitListener implements Listener {
             User user = userService.getUserByUUID(player.getUniqueId()).orElse(null);
             if (user == null) return;
 
+            Set<Destination> destinations = new HashSet<>();
+            destinations.add(Destination.CACHE);
+            destinations.add(Destination.STORAGE);
+            DataUpdateEvent dataUpdateEvent = new DataUpdateEvent(player, user, DataUpdateEvent.Cause.LEAVE, destinations);
+
             userTranslator.translateToUser(user, player);
-            userService.update(user, Destination.CACHE, Destination.STORAGE);
+            userService.update(user, destinations.toArray(new Destination[0]));
             userService.setTTLOfCacheByUUID(user.getUuid(), 600); // 10 minutes
         });
     }
