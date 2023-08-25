521ae0912a4ec1ac0798bb393a431666843a0031
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-25 13:21:49 +0200

    Add DefaultMessageConsumer

---
+++ src/main/java/net/smoothplugins/smoothsync/messenger/DefaultMessageConsumer.java
@@ -0,0 +1,11 @@
+package net.smoothplugins.smoothsync.messenger;
+
+import net.smoothplugins.smoothbase.messenger.MessageConsumer;
+
+public class DefaultMessageConsumer implements MessageConsumer {
+
+    @Override
+    public void consume(String message) {
+
+    }
+}
