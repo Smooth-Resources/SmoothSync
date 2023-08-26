4b69a543222fdda4628eaff6b25085c0c5d9602a
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-26 14:01:07 +0200

    Update DefaultMessageConsumer

--- src/main/java/net/smoothplugins/smoothsync/messenger/DefaultMessageConsumer.java
+++ src/main/java/net/smoothplugins/smoothsync/messenger/DefaultMessageConsumer.java
@@ -4,9 +4,13 @@ import com.google.inject.Inject;
 import net.smoothplugins.smoothbase.messenger.MessageConsumer;
 import net.smoothplugins.smoothbase.messenger.Messenger;
 import net.smoothplugins.smoothbase.serializer.Serializer;
+import net.smoothplugins.smoothsync.SmoothSync;
 import net.smoothplugins.smoothsync.messenger.message.DefaultMessage;
+import net.smoothplugins.smoothsync.messenger.message.QuitNotificationMessage;
 import net.smoothplugins.smoothsync.messenger.message.RequestUpdatedUserMessage;
 import net.smoothplugins.smoothsync.messenger.message.SendUpdatedUserMessage;
+import net.smoothplugins.smoothsync.user.UserSaver;
+import net.smoothplugins.smoothsyncapi.event.DataSyncEvent;
 import net.smoothplugins.smoothsyncapi.user.User;
 import net.smoothplugins.smoothsyncapi.user.UserService;
 import net.smoothplugins.smoothsyncapi.user.UserTranslator;
@@ -26,6 +30,10 @@ public class DefaultMessageConsumer implements MessageConsumer {
     private UserService userService;
     @Inject
     private UserTranslator userTranslator;
+    @Inject
+    private SmoothSync plugin;
+    @Inject
+    private UserSaver userSaver;
 
     @Override
     public void consume(String JSON, @Nullable UUID identifier) {
@@ -34,6 +42,11 @@ public class DefaultMessageConsumer implements MessageConsumer {
             case REQUEST_UPDATED_USER -> {
                 RequestUpdatedUserMessage message = serializer.deserialize(JSON, RequestUpdatedUserMessage.class);
                 consumeRequestUpdatedUserMessage(message, identifier);
+            }
+
+            case QUIT_NOTIFICATION -> {
+                QuitNotificationMessage message = serializer.deserialize(JSON, QuitNotificationMessage.class);
+                consumeQuitNotificationMessage(message);
             }
         }
     }
@@ -47,5 +60,30 @@ public class DefaultMessageConsumer implements MessageConsumer {
 
         SendUpdatedUserMessage sendUpdatedUserMessage = new SendUpdatedUserMessage(user);
         messenger.sendResponse(serializer.serialize(sendUpdatedUserMessage), identifier);
+    }
+
+    private void consumeQuitNotificationMessage(QuitNotificationMessage message) {
+        Player player = Bukkit.getPlayer(message.getUserUUID());
+        if (player == null || userSaver.containsPlayer(player)) return;
+
+        User user = userService.getUserByUUID(player.getUniqueId()).orElse(null);
+        if (user == null) return;
+
+        if (!userService.removeTTLFromCacheByUUID(user.getUuid())) {
+            userService.loadToCache(user);
+        }
+
+        applyData(user, player);
+    }
+
+    private void applyData(User user, Player player) {
+        Bukkit.getScheduler().runTask(plugin, () -> {
+            DataSyncEvent dataSyncEvent = new DataSyncEvent(player, user);
+            Bukkit.getPluginManager().callEvent(dataSyncEvent);
+            if (!dataSyncEvent.isCancelled()) {
+                userTranslator.translateToPlayer(user, player);
+                userSaver.addPlayer(player);
+            }
+        });
     }
 }
