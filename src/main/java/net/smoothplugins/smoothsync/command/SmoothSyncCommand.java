a553dac09176caa84e6dd352752144f86d6a6dba
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-26 18:14:41 +0200

    Add SmoothSyncCommand

---
+++ src/main/java/net/smoothplugins/smoothsync/command/SmoothSyncCommand.java
@@ -0,0 +1,59 @@
+package net.smoothplugins.smoothsync.command;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import net.smoothplugins.smoothbase.configuration.Configuration;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Locale;
+
+public class SmoothSyncCommand implements CommandExecutor, TabCompleter {
+
+    @Inject @Named("messages")
+    private Configuration messages;
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!sender.hasPermission("smoothsync.command.smoothsync")) {
+            sender.sendMessage(messages.getComponent("command.smoothsync.no-permission"));
+            return true;
+        }
+
+        if (args.length == 0) {
+            messages.getComponentList("command.smoothsync.help").forEach(sender::sendMessage);
+            return true;
+        }
+
+        switch (args[0].toLowerCase(Locale.ROOT)) {
+            case "reload" -> {
+
+            }
+
+            case "edit-inventory" -> {
+
+            }
+
+            case "edit-enderchest" -> {
+
+            }
+
+            default -> {
+                messages.getComponentList("command.smoothsync.help").forEach(sender::sendMessage);
+                return true;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return null;
+    }
+}
--- src/main/java/net/smoothplugins/smoothsync/loader/CommandLoader.java
+++ src/main/java/net/smoothplugins/smoothsync/loader/CommandLoader.java
@@ -2,13 +2,16 @@ package net.smoothplugins.smoothsync.loader;
 
 import com.google.inject.Inject;
 import net.smoothplugins.smoothsync.SmoothSync;
+import net.smoothplugins.smoothsync.command.SmoothSyncCommand;
 
 public class CommandLoader {
 
     @Inject
     private SmoothSync plugin;
+    @Inject
+    private SmoothSyncCommand smoothSyncCommand;
 
     public void load() {
-
+        plugin.getCommand("smoothsync").setExecutor(smoothSyncCommand);
     }
 }
