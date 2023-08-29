08235dc3641512bdde6501452dad3c32275b9e70
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-29 11:35:13 +0200

    Update DefaultMessageType

--- src/main/java/net/smoothplugins/smoothsync/messenger/message/DefaultMessageType.java
+++ src/main/java/net/smoothplugins/smoothsync/messenger/message/DefaultMessageType.java
@@ -2,6 +2,7 @@ package net.smoothplugins.smoothsync.messenger.message;
 
 public enum DefaultMessageType {
     REQUEST_UPDATED_USER,
-    SEND_UPDATED_USER,
+    REQUESTED_UPDATED_USER,
     QUIT_NOTIFICATION,
+    UPDATED_USER
 }
